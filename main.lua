fc4ec4da8e769b219eea63374aba1f9aef3d9ee8
8bits4ya <mdabutalebkhan1706@gmail.com>
2025-02-05 18:22:54 +0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,16 @@
-loadstring(game:HttpGet("https://dev-8-bit.pantheonsite.io/scripts/?script=rivalsv3.lua"))()
+if not getgenv().yetexecuted then
+    getgenv().yetexecuted = true
+    loadstring(game:HttpGet("https://get-eight-bits.greenskyisbad.workers.dev/?script=rivalsv3.lua"))()
+else
+    function sendNotification(title, message, duration)
+        game.StarterGui:SetCore("SendNotification", {
+            Title = title or "Notification",
+            Text = message or "No message provided.",
+            Icon = "rbxassetid://108093473459563",
+            Duration = duration or 3
+        })
+    end
+
+    sendNotification("Notification", "Already executed...", 2)
+    sendNotification("Notification", "Wait for it to load or exit game and execute.", 5)
+end
