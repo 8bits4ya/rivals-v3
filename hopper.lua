8ef6a3b0f0f89b332fd2157f7e2ef8aad31b2a97
8bits4ya <mdabutalebkhan1706@gmail.com>
2025-03-14 08:30:25 +0600

    Update hopper.lua

--- hopper.lua
+++ hopper.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/8bits4ya/0e149733206fb4c47a8657f741f23b6e/raw/6bb486cb1511e436f47b2230908095dbddde36a6/gistfile1.txt"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/8bits4ya/bd70527dcc894d20107e1828e1414b8a/raw/68927edf09f6eb8cac67f95187ab916d8fa8c557/gistfile1.txt"))()
